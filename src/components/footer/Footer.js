bd1b122fad7343274354b65218b819f7e2c456ce
Peter Gacek <peterwgacek@gmail.com>
2021-01-16 17:21:33 -0500

    Adds footer and header

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+
+import './Footer.css';
+
+export default function Footer(props) {
+    return (
+
+        <footer className="Footer">
+            <div className="links">
+                <a href="https://www.linkedin.com/in/peterwgacek/"><i class="fab fa-linkedin"></i></a>
+                <a href="https://github.com/peterwgacek"><i class="fab fa-github-square"></i></a>
+            </div>
+        </footer>
+
+    );
+}
---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+
+import { Link } from 'react-router-dom';
+
+
+export default function HomePage(props) {
+    return (
+        <main className="Page">
+
+            <img src="/favicon.png" alt="hero" className="headerlogo" />
+            <h1>
+                Welcome to
+                </h1>
+
+        </main>
+    );
+}

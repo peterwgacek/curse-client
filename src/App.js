c8488822870d83e5c3b9e1f0cd396f08d3632478
Peter Gacek <peterwgacek@gmail.com>
2021-01-19 17:44:45 -0500

    gets everything but app to render thru redux

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@
 import './App.css';
 // import { useSelector, useDispatch } from 'react-redux';
 import Response from './components/Response/Response';
-import { Provider } from 'react-redux';
 
 
 function App() {
 
   return (
     <div className="App">
-      <Provider store={store}>
-        <Response />
-      </Provider>
+      <Response />
     </div>
   );
 }

f93dbe560c015f8fe0759dc60fda2b96e0e7b373
Peter Gacek <peterwgacek@gmail.com>
2021-01-19 20:59:41 -0500

    Gets Redux working and ready for deploy

--- src/components/Response/Response.js
+++ src/components/Response/Response.js
@@ -56,7 +56,7 @@ const mapDispatchToProps = dispatch => {
 
 const mapStateToProps = state => {
     return {
-        currentNode: state.node
+        currentNode: state.currentNode
     }
 };
 
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -6,7 +6,6 @@ const reducer = (state = [], action) => {
         default:
             return state;
     };
-
 };
 
 export default reducer;
